56a557dcb4239f8e748a0a8f71be49c9d3780412
Alexandra DLG <adelagueronniere@gmail.com>
2018-05-17 11:55:38 +0200

    first push heroku

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module SuperLandingPageBoomerang
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
+    config.action_mailer.delivery_method = :mailjet_api
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'users/subscribe', to: "users#subscribe"
+  post 'users/unsubscribe/:id/:token', to: "users#unsubscribe", as: "unsubscribe_nl"
+  get 'users/unsubscribe/:id/:token', to: "users#unsubscribe"
+  post 'users/subscribe/:id/:token', to: "users#confirm_subscription", as: "subscribe_nl"
+  get 'users/subscribe/:id/:token', to: "users#confirm_subscription"
 end
