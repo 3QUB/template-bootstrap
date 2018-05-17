a22b05298aa7ec4d6951b3d46943784f8e704786
Quentin <berson.quentin@gmail.com>
2018-05-17 14:52:52 +0200

    route ajout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,15 @@
 class StaticPagesController < ApplicationController
+
   def home
   end
+
+  def landing_page_1
+  end
+
+  def landing_page_2
+  end
+
+  def landing_page_3
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get '1', to: 'static_pages#landing_page_1'
+  get '2', to: 'static_pages#landing_page_2'
+  get '3', to: 'static_pages#landing_page_3'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
