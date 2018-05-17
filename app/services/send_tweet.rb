77116aa0108050158f3c83584d4247cf88f5d959
aitbali <35599314+aitbali@users.noreply.github.com>
2018-05-17 18:05:51 +0200

    Update send_tweet.rb

--- app/services/send_tweet.rb
+++ app/services/send_tweet.rb
@@ -22,13 +22,13 @@ class SendTweet
 	def log_in_to_twitter
 		@client = Twitter::REST::Client.new do |config|
 			
-			config.consumer_key = "U8hreya881Y2YLCGukYzRerHP"
+			config.consumer_key = "P"
 
-			config.consumer_secret = "26Ed69rS9kbiDAgnoJqCbX6iRddRga9lFgZps2IhW9cKERUGkQ"
+			config.consumer_secret = ""
 
-			config.access_token = "995267863874285568-gZADxGg9iWkVRXgkkcyCZrOxCZUysyt"
+			config.access_token = ""
 
-			config.access_token_secret = "tHt5nMv4j6lLUEy6xOV7cEo7FBfdTM45YIuRL84noCOaW" 
+			config.access_token_secret = "" 
 
 		end
 	end
